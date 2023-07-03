f9559420832b84819ff19db7cebc4413f907dee0
Integrity Field <integritysunbear@gmail.com>
2023-07-03 03:17:09 -0400

    PageShell is fine

--- renderer/PageShell.jsx
+++ renderer/PageShell.jsx
@@ -1,104 +1,108 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import logo from './logo.svg'
-import './PageShell.css'
-import { PageContextProvider } from './usePageContext'
-import { Link } from './Link'
-import { childrenPropType } from './PropTypeValues'
+import React from 'react';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './PageShell.css';
+import { PageContextProvider } from './usePageContext';
+import { Link } from './Link';
+import { childrenPropType } from './PropTypeValues';
+import styled from 'styled-components';
 
-export { PageShell }
+const BlueBox = styled.div`
+	width: 112px;
+	height: 112px;
+	background: blue;
+`;
+
+export { PageShell };
 
 PageShell.propTypes = {
-  pageContext: PropTypes.any,
-  children: childrenPropType
-}
+	pageContext: PropTypes.any,
+	children: childrenPropType,
+};
 function PageShell({ pageContext, children }) {
-  return (
-    <React.StrictMode>
-      <PageContextProvider pageContext={pageContext}>
-        <Layout>
-          <Sidebar>
-            <Logo />
-            <Link className="navitem" href="/">
-              Home
-            </Link>
-            <Link className="navitem" href="/about">
-              About
-            </Link>
-          </Sidebar>
-          <Content>{children}</Content>
-        </Layout>
-      </PageContextProvider>
-    </React.StrictMode>
-  )
+	return (
+		<React.StrictMode>
+			<PageContextProvider pageContext={pageContext}>
+				<Layout>
+					<Sidebar>
+						<Logo />
+						<Link className='navitem' href='/'>
+							Home
+						</Link>
+						<Link className='navitem' href='/about'>
+							About
+						</Link>
+						<BlueBox />
+					</Sidebar>
+					<Content>{children}</Content>
+				</Layout>
+			</PageContextProvider>
+		</React.StrictMode>
+	);
 }
 
 Layout.propTypes = {
-  children: childrenPropType
-}
+	children: childrenPropType,
+};
 function Layout({ children }) {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        maxWidth: 900,
-        margin: 'auto'
-      }}
-    >
-      {children}
-    </div>
-  )
+	return (
+		<div
+			style={{
+				display: 'flex',
+				maxWidth: 900,
+				margin: 'auto',
+			}}>
+			{children}
+		</div>
+	);
 }
 
 Sidebar.propTypes = {
-  children: childrenPropType
-}
+	children: childrenPropType,
+};
 function Sidebar({ children }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        lineHeight: '1.8em'
-      }}
-    >
-      {children}
-    </div>
-  )
+	return (
+		<div
+			style={{
+				padding: 20,
+				flexShrink: 0,
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				lineHeight: '1.8em',
+			}}>
+			{children}
+		</div>
+	);
 }
 
 Content.propTypes = {
-  children: childrenPropType
-}
+	children: childrenPropType,
+};
 function Content({ children }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh'
-      }}
-    >
-      {children}
-    </div>
-  )
+	return (
+		<div
+			style={{
+				padding: 20,
+				paddingBottom: 50,
+				borderLeft: '2px solid #eee',
+				minHeight: '100vh',
+			}}>
+			{children}
+		</div>
+	);
 }
 
 function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10
-      }}
-    >
-      <a href="/">
-        <img src={logo} height={64} width={64} alt="logo" />
-      </a>
-    </div>
-  )
+	return (
+		<div
+			style={{
+				marginTop: 20,
+				marginBottom: 10,
+			}}>
+			<a href='/'>
+				<img src={logo} height={64} width={64} alt='logo' />
+			</a>
+		</div>
+	);
 }
