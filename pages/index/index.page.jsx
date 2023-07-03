d6f58304b938b37b5ce95be8d28cbdd73365cce3
Integrity Field <integritysunbear@gmail.com>
2023-07-03 02:51:51 -0400

    styled-components

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -1,18 +1,24 @@
-import { Counter } from './Counter'
+import styled from 'styled-components';
+import { Counter } from './Counter';
 
-export { Page }
+export { Page };
+
+const Container = styled.div`
+	width: 100%;
+	background-color: red;
+`;
 
 function Page() {
-  return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
-  )
+	return (
+		<Container>
+			<h1>Welcome</h1>
+			This page is:
+			<ul>
+				<li>Rendered to HTML.</li>
+				<li>
+					Interactive. <Counter />
+				</li>
+			</ul>
+		</Container>
+	);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import react from '@vitejs/plugin-react';
+import ssr from 'vite-plugin-ssr/plugin';
 
-export default {
-  plugins: [react(), ssr()]
-}
+const config = {
+	plugins: [react(), ssr()],
+	ssr: {
+		noExternal: ['styled-components'],
+	},
+};
+
+export default config;
